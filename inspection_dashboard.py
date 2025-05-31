ba4c5685e114363ae9d1e1a872f538d77c69f147
Squirrellzy <78796513+Squirrellzy@users.noreply.github.com>
2025-05-31 10:42:46 -0400

    Update inspection_dashboard.py

--- inspection_dashboard.py
+++ inspection_dashboard.py
@@ -30,7 +30,8 @@ if "authenticated" not in st.session_state:
 # Login
 if not st.session_state.authenticated:
     st.title("🔐 Login Required")
-
+    with right_col:
+        st.image(f"data:image/png;base64,{logo_base64}", width=200)
     with st.form("login_form"):
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
